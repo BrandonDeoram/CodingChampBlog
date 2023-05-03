b47fbfa90c72fb1d588d8d5f0ac5d46e8be63cc7
BrandonDeoram <brandondeoram8931@gmail.com>
2023-05-02 22:36:53 -0400

    added nav bar

--- with-tailwindcss-app/components/Header.jsx
+++ with-tailwindcss-app/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "next/link";
-
+const categories = [
+  { name: "react", slug: "react" },
+  { name: "nextjs", slug: "nextjs" },
+];
 const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -12,7 +15,15 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="hidden md:float-left md:contents"></div>
+        <div className="hidden md:float-right md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
