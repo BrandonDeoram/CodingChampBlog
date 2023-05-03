6cc1f3d414f23f62544273a55aec2b21a4297430
BrandonDeoram <brandondeoram8931@gmail.com>
2023-05-02 23:06:09 -0400

    working on posts'

--- with-tailwindcss-app/pages/index.js
+++ with-tailwindcss-app/pages/index.js
@@ -1,16 +1,7 @@
 import Head from "next/head";
+import { getPosts } from "../services";
 import { PostCard, Categories, PostWidget } from "../components";
-export default function Home() {
-  const posts = [
-    {
-      title: "React Testing",
-      excerpt: "Learn React Testing",
-    },
-    {
-      title: "React with Tailwind",
-      excerpt: "Learn React with Tailwind",
-    },
-  ];
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
@@ -22,7 +13,7 @@ export default function Home() {
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
             <div className="" key={index}>
-              <PostCard post={post} key={post.title}></PostCard>
+              <PostCard post={post.node} key={post.title}></PostCard>
             </div>
           ))}
         </div>
@@ -36,4 +27,11 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
