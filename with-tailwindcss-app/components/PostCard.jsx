db023b4662ad596d6e008d5fea112bd05cb5ba8e
BrandonDeoram <brandondeoram8931@gmail.com>
2023-05-04 22:47:19 -0400

    editing post

--- with-tailwindcss-app/components/PostCard.jsx
+++ with-tailwindcss-app/components/PostCard.jsx
@@ -48,7 +48,7 @@ const PostCard = ({ post }) => {
           </span>
         </div>
       </div>
-      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8 hidden lg:block">
         {post.excerpt}
       </p>
       <div className="text-center">
