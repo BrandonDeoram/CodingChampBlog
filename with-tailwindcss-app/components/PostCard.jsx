a80d44752a924efbf4b574f775f3e3dd1964f038
BrandonDeoram <brandondeoram8931@gmail.com>
2023-05-14 13:30:38 -0400

    edited reponsive card

--- with-tailwindcss-app/components/PostCard.jsx
+++ with-tailwindcss-app/components/PostCard.jsx
@@ -12,11 +12,11 @@ const PostCard = ({ post }) => {
           className="object-top absolute h-40 w-full object-contain rounded-t-lg lg:rounded-lg "
         />
       </div>
-      <h1 className="transition duration-300 text-center mb-8 cursor-pointer text-lightGreyBlue  text-2xl font-semibold">
+      <h1 className="transition duration-300 text-center mb-8 cursor-pointer text-lightGreyBlue  text-2xl font-semibold m-2">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 ">
           <img
             src={post.author.photo.url}
             alt={post.author.name}
--- with-tailwindcss-app/components/PostWidget.jsx
+++ with-tailwindcss-app/components/PostWidget.jsx
@@ -20,12 +20,15 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8 ">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         {slug ? "Related Posts" : "Recent Posts"}
       </h3>
       {relatedPosts.map((post, index) => (
-        <div key={index} className="flex items-center w-full mb-4 hover:bg-buttonHover p-3 rounded-lg transition duration-200">
+        <div
+          key={index}
+          className="flex items-center w-full mb-4 hover:bg-buttonHover p-3 rounded-lg transition duration-200"
+        >
           <div className="w-16 flex-none">
             <img
               alt={post.title}
