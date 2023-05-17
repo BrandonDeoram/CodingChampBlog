fc326530f97ec40a4712471f6cd70b2e636993c2
BrandonDeoram <brandondeoram8931@gmail.com>
2023-05-16 21:09:46 -0400

    added tiktok

--- with-tailwindcss-app/pages/about.js
+++ with-tailwindcss-app/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react";
-import me from "../public/me.jpg";
+import tiktok from "../public/tiktok.png";
+import Image from "next/image";
 const About = () => {
   return (
     <div className="container mx-auto p-10 flex-col items-center justify-center">
@@ -11,17 +12,47 @@ const About = () => {
           className="mx-auto w-36 h-36 rounded-full border-4 border-buttonHover"
         />
         <p className="my-4 text-2xl font-semibold sm:text-4xl">About Me</p>
-        <p className="px-4 md:px-6 pt-5 pb-24 md:pb-44 max-w-[900px] mx-auto">
+        <p className="px-4 md:px-6 pt-5 pb-5 md:pb-1 max-w-[900px] mx-auto">
           Hi there! I'm Sanjay, a Full Stack Developer who loves coding projects
-          that benefit both myself and others. I recently graduated from Ontario Tech University with a major in
-          Computer Science.Apart from work and school, I enjoy creating YouTube videos, including
-          study vlogs and developer vlogs, to document my coding project
-          journey.
+          that benefit both myself and others. I recently graduated from Ontario
+          Tech University with a major in Computer Science.Apart from work and
+          school, I enjoy creating YouTube videos, including study vlogs and
+          developer vlogs, to document my coding project journey.
           <br />
           <br />
         </p>
 
-        <div>More content</div>
+        <div className="flex items-center justify-center content-center gap-5">
+          <img
+            src={
+              "https://w7.pngwing.com/pngs/986/124/png-transparent-tiktok-social-media-logos-brands-icon-thumbnail.png"
+            }
+            onClick={() => window.open("https://www.tiktok.com/@sanjaydeoram")}
+            alt="tiktok"
+            className="w-10 h-10 opacity-50"
+          />
+          <img
+            src={
+              "https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png"
+            }
+            alt="linkedin"
+            className="w-10 h-10"
+          />
+        </div>
+        <div className="flex items-center justify-center content-center gap-5 flex-wrap pt-10">
+          <button
+            className="text-black lg:ml-4 cursor-pointer hover:opacity-70 bg-buttonHover p-3 rounded-lg transition duration-200"
+            onClick={() => window.open("mailto:sanjay.deoram@ontariotechu.net")}
+          >
+            sanjay.deoram@ontariotechu.net
+          </button>
+          <button
+            className="text-black lg:ml-4 cursor-pointer hover:opacity-70 bg-buttonHover p-3 rounded-lg transition duration-200"
+            onClick={() => window.open("https://github.com/BrandonDeoram")}
+          >
+            See Github Projects
+          </button>
+        </div>
       </div>
     </div>
   );
